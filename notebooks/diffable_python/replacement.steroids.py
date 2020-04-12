cfefc2d9597542aeae20b16762457ab128e19002
Brian <brianmkcebm@gmail.com>
2020-04-12 14:01:26 +0100

    update steroids
    update list based on LSHTM feedback
    investigate restricting by form_route

--- notebooks/diffable_python/replacement.steroids.py
+++ notebooks/diffable_python/replacement.steroids.py
@@ -14,7 +14,7 @@
 #     name: python3
 # ---
 
-# [PHE criteria for shielded patients](https://www.gov.uk/government/publications/covid-19-guidance-on-social-distancing-and-for-vulnerable-people/guidance-on-social-distancing-for-everyone-in-the-uk-and-protecting-older-people-and-vulnerable-adults) specifies people with weakened immune systems and goes on to give an example of "_steroid tablets_". This is very unspecific as you can have injections, enemeas, inhaled which can all act on the imune system. The [BNF hsa a useful summary](https://bnf.nice.org.uk/treatment-summary/corticosteroids-general-use.html) of steroids including area of action, differnt types, uses and side-effects. In this list we will include "replacement like" steroids. 
+# [PHE criteria for shielded patients](https://www.gov.uk/government/publications/covid-19-guidance-on-social-distancing-and-for-vulnerable-people/guidance-on-social-distancing-for-everyone-in-the-uk-and-protecting-older-people-and-vulnerable-adults) specifies people with weakened immune systems and goes on to give an example of "_steroid tablets_". This is very unspecific as you can have injections, enemeas, inhaled which can all act on the imune system. The [BNF has a useful summary](https://bnf.nice.org.uk/treatment-summary/corticosteroids-general-use.html) of steroids including area of action, differnt types, uses and side-effects. In this list we will include "replacement like" steroids. 
 #
 
 from ebmdatalab import bq
@@ -24,13 +24,14 @@ import pandas as pd
 # +
 sql = '''WITH bnf_codes AS (
   SELECT bnf_code FROM hscic.presentation WHERE 
-    bnf_code LIKE '060302%' OR #BNF  glucocorticoid
-    bnf_code LIKE '060301%'     #BNF replacement therapy - fludrocortisone
-  
+    bnf_code LIKE '060302%'       OR    #BNF  glucocorticoid
+    bnf_code LIKE '060301%'       OR    #BNF replacement therapy - fludrocortisone
+    bnf_code LIKE '100102%'       OR      #BNF paragraph on corticosteroinds in rheumatic disease
+    bnf_code LIKE '010502%'             #BNF paragraph on corticosteroinds in bowel disorders
   )
 
 SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
+FROM dmd.vmp_full
 WHERE bnf_code IN (SELECT * FROM bnf_codes)
 
 UNION ALL
@@ -43,7 +44,47 @@ ORDER BY type, bnf_code, id'''
 
 replacement_steroids_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','replacement_steroids_codelist.csv'))
 pd.set_option('display.max_rows', None)
-replacement_steroids_codelist
-# -
+pd.set_option('display.max_colwidth', None)
+replacement_steroids_codelist.count()
+# +
+sql = '''WITH bnf_codes AS (
+  SELECT bnf_code FROM hscic.presentation WHERE 
+    bnf_code LIKE '060302%'       OR    #BNF  glucocorticoid
+    bnf_code LIKE '060301%'       OR    #BNF replacement therapy - fludrocortisone
+    bnf_code LIKE '100102%'       OR      #BNF paragraph on corticosteroinds in rheumatic disease
+    bnf_code LIKE '010502%'             #BNF paragraph on corticosteroinds in bowel disorders
+  )
+
+SELECT obj_type, snomed_id, bnf_code, dmd_name, form_route
+FROM ebmdatalab.measures_v2.dmd_objs_with_form_route
+WHERE bnf_code IN (SELECT * FROM bnf_codes) AND
+(obj_type = "vmp" OR obj_type = "amp")
 
 
+ORDER BY obj_type, bnf_code'''
+
+test = bq.cached_read(sql, csv_path=os.path.join('..','data','test.csv'))
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+test.count()
+
+
+# +
+sql = '''
+SELECT obj_type, snomed_id, bnf_code, dmd_name, form_route
+FROM ebmdatalab.measures_v2.dmd_objs_with_form_route
+WHERE 
+    (bnf_code LIKE '060302%'       OR    #BNF  glucocorticoid
+    bnf_code LIKE '060301%'       OR    #BNF replacement therapy - fludrocortisone
+    bnf_code LIKE '100102%'       OR      #BNF paragraph on corticosteroinds in rheumatic disease
+    bnf_code LIKE '010502%' )            #BNF paragraph on corticosteroinds in bowel disorders
+AND
+(obj_type = "vmp" OR obj_type = "amp")
+
+
+ORDER BY obj_type, bnf_code'''
+
+test2 = bq.cached_read(sql, csv_path=os.path.join('..','data','test2.csv'))
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+test2.count()
